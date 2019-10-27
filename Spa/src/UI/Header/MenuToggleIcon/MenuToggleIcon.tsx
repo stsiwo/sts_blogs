75344b337e979337ee928e47c2a6a3281fd8d08a
sts-ubuntu <stsiwo@gmail.com>
2019-10-27 02:06:37 -0700

    added event handlers

--- Spa/src/UI/Header/MenuToggleIcon/MenuToggleIcon.tsx
+++ Spa/src/UI/Header/MenuToggleIcon/MenuToggleIcon.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import './MenuToggleIcon.scss';
 import { useResponsiveComponent } from '../../Base/Hooks/ResponsiveComponentHook';
+import { useDispatch } from 'react-redux';
+import { toggleNavBarActionCreator } from '../../../actions/creators';
 
 
 const MenuToggleIcon: React.FunctionComponent<{}> = (props: {}) => {
 
+    const dispatch = useDispatch()
+
+    const handleMenuToggleIconEvent: React.EventHandler<React.MouseEvent> = (e) => {
+      dispatch(toggleNavBarActionCreator(true))
+    }
+
     return (
-      <i className="header-menu-toggle-icon">
+      <i className="header-menu-toggle-icon" onClick={handleMenuToggleIconEvent}>
         <div className="">MenuToggleIcon</div> 
       </i>
     );
--- Spa/src/UI/Header/MenuWrapper/MenuWrapper.tsx
+++ Spa/src/UI/Header/MenuWrapper/MenuWrapper.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import './MenuWrapper.scss';
 import Menu from './Menu/Menu';
+import { useSelector, useDispatch } from 'react-redux';
+import { StateType } from '../../../states/types';
+import { toggleLoginFormActionCreator } from '../../../actions/creators';
 
 
 const MenuWrapper: React.FunctionComponent<{}> = (props: {}) => {
 
-    return (
-      <div className="header-menu-wrapper">
+    const isNavBarOpen = useSelector((state: StateType) => state.ui.isLoginFormOpen)
+    const dispatch = useDispatch()
+
+    const handleNavBarOutsideClickEvent: React.EventHandler<React.MouseEvent<HTMLDivElement>> = (e) => {
+      dispatch(toggleLoginFormActionCreator(false))
+    }
+
+    return isNavBarOpen && (
+      <div className="header-menu-wrapper" onClick={handleNavBarOutsideClickEvent}>
         <Menu />
       </div>
     );
