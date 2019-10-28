f50c1b4430834a2303257606edddd88e8712bca7
sts-ubuntu <stsiwo@gmail.com>
2019-10-27 22:41:14 -0700

    added identity-obj-proxy to mock scss module

--- Spa/jest.config.js
+++ Spa/jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "<rootDir>/src/"
   ],
   "testRegex": "\\.(spec|test)\\.tsx?$",
+  "moduleNameMapper": {
+      "\\.(css|scss|less)$": "identity-obj-proxy"
+    },
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
