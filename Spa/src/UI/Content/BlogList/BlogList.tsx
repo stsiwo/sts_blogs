755d63bc7e075e9cda258a4f0c0e8c1d3ff96dcc
sts-ubuntu <stsiwo@gmail.com>
2019-10-29 01:36:30 -0700

    rename css blog name to blog-list

--- Spa/src/UI/Content/BlogList/BlogList.tsx
+++ Spa/src/UI/Content/BlogList/BlogList.tsx
@@ -16,7 +16,7 @@ const BlogList: React.FunctionComponent<{}> = (props: {}) => {
   const isFilterSortBarOpen = useSelector((state: StateType) => state.ui.isFilterSortBarOpen)
 
   const renderTags = (tagList: TagType[]): React.ReactNode => {
-    return tagList.map((tag: TagType) => <div className="blog-filter-tags-tag" key={tag.id}>{tag.name}</div>)
+    return tagList.map((tag: TagType) => <div className="blog-list-filter-tags-tag" key={tag.id}>{tag.name}</div>)
   }
 
   const currentWidth = useResponsiveComponent()
@@ -35,63 +35,63 @@ const BlogList: React.FunctionComponent<{}> = (props: {}) => {
   const renderBlogLists = (blogList: BlogType[]): React.ReactNode => {
     return blogList.map((blog: BlogType) => {
       return (
-        <div className="blog-items-item-wrapper" key={blog.id}></div>
+        <div className="blog-list-items-item-wrapper" key={blog.id}></div>
       )
     })
   }
 
   return (
-    <div className="blog-wrapper">
+    <div className="blog-list-wrapper">
       {(currentWidth > cssGlobal.tabletSize || (isFilterSortBarOpen && currentWidth <= cssGlobal.tabletSize)) &&
-        <aside className="blog-aside-wrapper">
+        <aside className="blog-list-aside-wrapper">
         {currentWidth <= cssGlobal.tabletSize && 
-          <div className="blog-filter-sort-close-icon-wrapper">
-        <i className="blog-filter-sort-close-icon" onClick={handleFilterSortNavCloseClickEvent} >&#10006;</i>
+          <div className="blog-list-filter-sort-close-icon-wrapper">
+        <i className="blog-list-filter-sort-close-icon" onClick={handleFilterSortNavCloseClickEvent} >&#10006;</i>
           </div>
          }
-          <div className="blog-filter-wrapper">
-            <h3 className="blog-filter-title">Filter</h3>
-            <div className="blog-filter-tags-wrapper">
-              <h4 className="blog-filter-tags-title">Tags</h4>
+          <div className="blog-list-filter-wrapper">
+            <h3 className="blog-list-filter-title">Filter</h3>
+            <div className="blog-list-filter-tags-wrapper">
+              <h4 className="blog-list-filter-tags-title">Tags</h4>
               {renderTags(getTagTestData())}
             </div>
-            <div className="blog-filter-date-wrapper">
-              <h4 className="blog-filter-date-title">Date</h4>
+            <div className="blog-list-filter-date-wrapper">
+              <h4 className="blog-list-filter-date-title">Date</h4>
             </div>
           </div>
-          <div className="blog-sort-wrapper">
-            <h3 className="blog-sort-title">Sort</h3>
-            <div className="blog-filter-sort-item-wrapper">
-              <input type="checkbox" className="blog-filter-sort-item-input" id="checkbox_id" value="value" />
-              <label htmlFor="checkbox_id" className="blog-filter-sort-item-label">Text</label>
+          <div className="blog-list-sort-wrapper">
+            <h3 className="blog-list-sort-title">Sort</h3>
+            <div className="blog-list-filter-sort-item-wrapper">
+              <input type="checkbox" className="blog-list-filter-sort-item-input" id="checkbox_id" value="value" />
+              <label htmlFor="checkbox_id" className="blog-list-filter-sort-item-label">Text</label>
             </div>
-            <div className="blog-filter-sort-item-wrapper">
-              <input type="checkbox" className="blog-filter-sort-item-input" id="checkbox_id" value="value" />
-              <label htmlFor="checkbox_id" className="blog-filter-sort-item-label">Text</label>
+            <div className="blog-list-filter-sort-item-wrapper">
+              <input type="checkbox" className="blog-list-filter-sort-item-input" id="checkbox_id" value="value" />
+              <label htmlFor="checkbox_id" className="blog-list-filter-sort-item-label">Text</label>
             </div>
-            <div className="blog-filter-sort-item-wrapper">
-              <input type="checkbox" className="blog-filter-sort-item-input" id="checkbox_id" value="value" />
-              <label htmlFor="checkbox_id" className="blog-filter-sort-item-label">Text</label>
+            <div className="blog-list-filter-sort-item-wrapper">
+              <input type="checkbox" className="blog-list-filter-sort-item-input" id="checkbox_id" value="value" />
+              <label htmlFor="checkbox_id" className="blog-list-filter-sort-item-label">Text</label>
             </div>
           </div>
         </aside>
       }
-      <section className="blog-section-wrapper">
-        <h2 className="blog-title">BlogLists</h2>
-        <div className="blog-items-wrapper">
+      <section className="blog-list-section-wrapper">
+        <h2 className="blog-list-title">BlogLists</h2>
+        <div className="blog-list-items-wrapper">
           {renderBlogLists(getBlogTestData())}
         </div>
-        <div className="blog-pagination-wrapper">
-          <button className="blog-pagination-btn blog-pagination-to-first-btn" value='1'>&laquo;</button>
-          <button className="blog-pagination-btn" value='1'>1</button>
-          <button className="blog-pagination-btn" value='2'>2</button>
-          <button className="blog-pagination-btn" value='3'>3</button>
-          <button className="blog-pagination-btn" value='4'>4</button>
-          <button className="blog-pagination-btn" value='5'>5</button>
-          <button className="blog-pagination-btn blog-pagination-to-last-btn" value='last'>&raquo;</button>
+        <div className="blog-list-pagination-wrapper">
+          <button className="blog-list-pagination-btn blog-list-pagination-to-first-btn" value='1'>&laquo;</button>
+          <button className="blog-list-pagination-btn" value='1'>1</button>
+          <button className="blog-list-pagination-btn" value='2'>2</button>
+          <button className="blog-list-pagination-btn" value='3'>3</button>
+          <button className="blog-list-pagination-btn" value='4'>4</button>
+          <button className="blog-list-pagination-btn" value='5'>5</button>
+          <button className="blog-list-pagination-btn blog-list-pagination-to-last-btn" value='last'>&raquo;</button>
         </div>
       </section>
-      {currentWidth <= cssGlobal.tabletSize && <i className="blog-filter-sort-icon" onClick={handleFilterSortNavClickEvent} />}
+      {currentWidth <= cssGlobal.tabletSize && <i className="blog-list-filter-sort-icon" onClick={handleFilterSortNavClickEvent} />}
     </div>
   );
 }
