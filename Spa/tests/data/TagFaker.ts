8a63545507d2697e36f1d9e967e5a492199b5bee
sts-ubuntu <stsiwo@gmail.com>
2019-10-28 21:28:06 -0700

    added Tag domain and its test data seeder

---
+++ Spa/tests/data/TagFaker.ts
@@ -0,0 +1,16 @@
+import { TagType } from "../../src/domain/tag/TagType";
+import * as faker from 'faker';
+
+export const getTagTestData = (num: number = 10): TagType[] => {
+
+  var tagList: TagType[] = []
+
+  for(var i = 1; i <= num; i++) {
+    tagList.push({
+      id: i.toString(),
+      name: faker.name.title()
+    })
+  }
+
+  return tagList
+}
