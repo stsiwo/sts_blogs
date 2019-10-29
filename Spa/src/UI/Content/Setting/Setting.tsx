1f28afa78564b2f9bc9e1994c8139cb4e0fbf4e4
sts-ubuntu <stsiwo@gmail.com>
2019-10-29 04:07:51 -0700

    added BlogManagement Component to Setting Component (dynamic Component rendering)

--- Spa/src/UI/Content/Setting/Setting.tsx
+++ Spa/src/UI/Content/Setting/Setting.tsx
@@ -4,7 +4,7 @@ import { useResponsiveComponent } from '../../Base/Hooks/ResponsiveComponentHook
 import { useCssGlobalContext } from '../../Base/Context/CssGlobalContext/CssGlobalContext';
 import { Link, Route } from 'react-router-dom';
 import Profile from './Profile/Profile';
-import { SettingContentComponents } from './SettingContentComponents';
+import { SettingContentComponents, SettingContentComponentsType } from './SettingContentComponents';
 
 
 const Setting: React.FunctionComponent<{}> = (props: {}) => {
@@ -12,7 +12,7 @@ const Setting: React.FunctionComponent<{}> = (props: {}) => {
   const [currentSettingContent, setSettingContent] = React.useState("Profile");
 
   const renderSettingContent: (componentName: string) => React.ReactNode = (componentName) => {
-    const ComponentName: React.ComponentType = SettingContentComponents[componentName];
+    const ComponentName: React.ComponentType = SettingContentComponents.find((settingContentComponent: SettingContentComponentsType) => settingContentComponent.label.localeCompare(componentName) === 0).component
     return <ComponentName />;
   }
 
@@ -21,37 +21,21 @@ const Setting: React.FunctionComponent<{}> = (props: {}) => {
     setSettingContent(selectedSettingItem);
   }
 
+  const renderSettingContentList = (): React.ReactNode => {
+    return SettingContentComponents.map((settingContentComponent: SettingContentComponentsType) => {
+      return (
+          <li className="setting-list-li" onClick={handleSettingListClick} key={settingContentComponent.label}>
+            {settingContentComponent.label}
+          </li>
+      )
+    })
+  }
+
   return (
     <div className="setting-wrapper">
       <aside className="setting-list-wrapper">
         <ul className="setting-list-ul">
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
-          <li className="setting-list-li" onClick={handleSettingListClick} >
-            Profile
-          </li>
+          {renderSettingContentList()}
         </ul>
       </aside>
       <article className="setting-content">
--- Spa/src/UI/Content/Setting/SettingContentComponents.tsx
+++ Spa/src/UI/Content/Setting/SettingContentComponents.tsx
@@ -1,17 +1,25 @@
 import SettingHome from "./SettingHome/SettingHome";
 import Profile from "./Profile/Profile";
 import { ComponentType } from "react";
+import BlogManagement from "./BlogManagement/BlogManagement";
 
 export declare type SettingContentComponentsType = {
-  [key: string]: ComponentType;
+  label: string
+  component: ComponentType
 }
 
 /* must match with innerHTML in <li> */
-export const SettingContentComponents: SettingContentComponentsType = {
-  SettingHome: SettingHome,
-  Profile: Profile,
-}
-
-const variable = 'settingHome';
-
-console.log(SettingContentComponents[variable]);
+export const SettingContentComponents: SettingContentComponentsType[] = [ 
+  {
+    label: 'Home',
+    component: SettingHome,
+  },
+  {
+    label: 'Profile',
+    component: Profile,
+  },
+  {
+    label: 'Blogs',
+    component: BlogManagement,
+  },
+]
