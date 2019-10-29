44f6934c88fac15d54663c5cb4add93d457345cf
sts-ubuntu <stsiwo@gmail.com>
2019-10-29 06:20:02 -0700

    added Icon and Pagination Base Component

---
+++ Spa/src/UI/Base/Components/Icon/Icon.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './Icon.scss';
+import { IconPropType } from './types';
+
+const Icon: React.FunctionComponent<IconPropType> = (props: IconPropType) => {
+
+  return (
+    <div className={`icon-wrapper ${props.css}`}>
+      <i className="icon-item">{props.label}</i> 
+    </div>
+    );
+  }
+  
+  export default Icon;
+  
+  
+  
+  
+  
+
