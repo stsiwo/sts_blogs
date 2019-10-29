d75c9b964a5d7a3a8147129d1b60acfbf16bde90
sts-ubuntu <stsiwo@gmail.com>
2019-10-29 03:00:27 -0700

    added Blog Component and its css

--- Spa/src/UI/Content/Blog/Blog.tsx
+++ Spa/src/UI/Content/Blog/Blog.tsx
@@ -1,14 +1,37 @@
 import * as React from 'react';
 import './Blog.scss';
 import { useParams } from 'react-router';
+import { BlogType } from '../../../domain/blog/BlogType';
+import { getBlogTestData } from '../../../../tests/data/BlogFaker';
+import { TagType } from '../../../domain/tag/TagType';
+import { getTagTestData } from '../../../../tests/data/TagFaker';
 
 const Blog: React.FunctionComponent<{}> = (props: {}) => {
 
   let { id } = useParams()
 
+  const testBlog: BlogType = getBlogTestData(1)[0]
+
+  const testTags: TagType[] = getTagTestData(20)
+
+  const renderTags = (tagList: TagType[]): React.ReactNode => {
+    return tagList.map((tag: TagType) => <div className="tag-icon" key={tag.id}>{tag.name}</div>)
+  }
+
   return (
     <div className="blog-wrapper">
-      <h1>{id}</h1>
+      <h1 className="blog-title">{testBlog.title}</h1>
+      <h2 className="blog-subtitle">{testBlog.subTitle}</h2>
+      <div className="blog-tags-wrapper">
+        {renderTags(testTags)}
+      </div>
+      <div className="blog-author-wrapper">
+        <img src="" alt="author avatar" className="blog-author-avatar"/>
+        <h3 className="blog-author-name">author name</h3>
+      </div>
+      <div className="blog-content-wrapper">
+        {testBlog.content}
+      </div>
     </div>
     );
   }
