f61466229b845f1b919f869a0ff20e1468480d37
sts-ubuntu <stsiwo@gmail.com>
2019-10-29 04:07:02 -0700

    added BlogManagement Component in Setting

---
+++ Spa/src/UI/Content/Setting/BlogManagement/BlogManagement.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './BlogManagement.scss';
+
+
+const BlogManagement: React.FunctionComponent<{}> = (props: {}) => {
+
+  return (
+    <div className="setting-blog-management-wrapper">
+      blog-management
+    </div>
+  );
+}
+
+export default BlogManagement;
+
+
+
+
