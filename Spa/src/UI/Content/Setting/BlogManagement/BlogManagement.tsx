562ff888c0c786d53cfe5ce0a923967e90655446
sts-ubuntu <stsiwo@gmail.com>
2019-10-30 09:06:07 -0700

    added create new blog link, Tags filter input, keywords input css

--- Spa/src/UI/Content/Setting/BlogManagement/BlogManagement.tsx
+++ Spa/src/UI/Content/Setting/BlogManagement/BlogManagement.tsx
@@ -4,26 +4,66 @@ import { BlogType } from '../../../../domain/blog/BlogType';
 import { getBlogTestData } from '../../../../../tests/data/BlogFaker';
 import { dateFormatOption } from '../../../../utils';
 import Icon from '../../../Base/Components/Icon/Icon';
+import { getTagTestData } from '../../../../../tests/data/TagFaker';
+import Tag from '../../../Base/Components/Tag/Tag';
+import { Link } from 'react-router-dom';
 
+
+declare type TagType = {
+  name: string
+}
 
 const BlogManagement: React.FunctionComponent<{}> = (props: {}) => {
 
-  const controllerRefs: Map<string, HTMLDivElement> = new Map()
+
+  const [currentTags, setTag] = React.useState([{ name: 'sampletag' }])
+  const [currentBlogs, setBlogs] = React.useState(getBlogTestData())
+  const controllerRefs: Map<string, React.MutableRefObject<HTMLDivElement>> = new Map()
+  const tagInputRef = React.useRef(null)
+
+  console.log(currentTags)
 
   const handleBlogControllerOpenClickEvent: React.EventHandler<React.MouseEvent<HTMLButtonElement>> = (e) => {
-    controllerRefs.get(e.currentTarget.id).style.display = 'flex';
+    controllerRefs.get(e.currentTarget.id).current.style.display = 'flex';
   }
 
   const handleBlogControllerCloseClickEvent: React.EventHandler<React.MouseEvent<HTMLButtonElement>> = (e) => {
-    controllerRefs.get(e.currentTarget.id).style.display = 'none';
+    controllerRefs.get(e.currentTarget.id).current.style.display = 'none';
   }
 
-  const setControllerRef = (element: HTMLDivElement) => {
-    controllerRefs.set(element.id, element)
+  //  const setControllerRef = (element: HTMLDivElement) => {
+  //    console.log(element)
+  //    controllerRefs.set(element.id, element)
+  //  }
+
+  const renderCurrentTags = () => {
+    return currentTags.map((tag: TagType) => {
+      return (
+        <Tag name={tag.name} withCancelBtn />
+      )
+    })
+  }
+
+  const handleTagInputEnterOrTabKeyClickEvent: React.EventHandler<React.KeyboardEvent<HTMLInputElement>> = (e) => {
+
+    if (e.currentTarget.value === "") return false
+
+    if (e.key == 'Enter' || e.key == 'Tab') {
+      console.log("you entered")
+      console.log(e.currentTarget.value)
+      setTag([
+        ...currentTags,
+        { name: e.currentTarget.value }
+      ])
+      e.currentTarget.value = ""
+    }
   }
 
   const renderBlogs = (blogs: BlogType[]): React.ReactNode => {
+
     return blogs.map((blog: BlogType) => {
+      const divRef = React.useRef(null)
+      controllerRefs.set(blog.id, divRef)
       return (
         <div className="blog-management-item-wrapper" key={blog.id}>
           <img src="" alt="blog item" className="blog-management-item-img" />
@@ -32,7 +72,7 @@ const BlogManagement: React.FunctionComponent<{}> = (props: {}) => {
           <div className="blog-management-btn-wrapper">
             <button className="blog-management-item-controller" onClick={handleBlogControllerOpenClickEvent} id={`blog-${blog.id}`}>&hellip;</button>
           </div>
-          <div className="blog-management-item-controller-wrapper" ref={setControllerRef} id={`blog-${blog.id}`}>
+          <div className="blog-management-item-controller-wrapper" ref={divRef} id={`blog-${blog.id}`}>
             <button className="blog-management-item-edit-btn">Edit</button>
             <button className="blog-management-item-delete-btn">Delete</button>
             <button className="blog-management-item-close-btn" onClick={handleBlogControllerCloseClickEvent} id={`blog-${blog.id}`}>&#10005;</button>
@@ -47,7 +87,7 @@ const BlogManagement: React.FunctionComponent<{}> = (props: {}) => {
       <div className="blog-management-main-wrapper">
         <h2 className="blog-management-title">Blog Management</h2>
         <div className="blog-management-items-wrapper">
-          {renderBlogs(getBlogTestData(20))}
+          {renderBlogs(currentBlogs)}
         </div>
         <div className="blog-management-pagination-wrapper">
           pagination
@@ -56,12 +96,27 @@ const BlogManagement: React.FunctionComponent<{}> = (props: {}) => {
       <aside className="blog-management-aside-wrapper">
         <ul className="blog-management-aside-ul">
           <li className="blog-management-aside-li">
-            Create New Blog
+            <Link to="./" className="blog-management-aside-new-blog-link">
+              Create New Blog
+            </Link>
           </li>
           <li className="blog-management-aside-li">
-            Filter Blogs
+            <h3 className="blog-management-aside-filter-title">Filter Blogs</h3>
+            <div className="blog-management-aside-filter-tags-wrapper" >
+              <h4 className="blog-management-aside-filter-tags-title">Tags</h4>
+              <input type="text" className="blog-management-aside-filter-tags-input" onKeyDown={handleTagInputEnterOrTabKeyClickEvent} ref={tagInputRef}/>
+              {renderCurrentTags()}
+            </div>
+            <div className="blog-management-aside-filter-keyword-wrapper" >
+              <h4 className="blog-management-aside-filter-keyword-title">Keywords</h4>
+              <input type="text" className="blog-management-aside-filter-keyword-input" />
+            </div>
+            <div className="blog-management-aside-filter-date-wrapper" >
+              <h4 className="blog-management-aside-filter-date-title">Date</h4>
+            </div>
           </li>
           <li className="blog-management-aside-li">
+            <h3 className="blog-management-aside-sort-title">Sort Blogs</h3>
             Sort Blogs
           </li>
         </ul>
