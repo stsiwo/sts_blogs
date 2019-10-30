172f46cb2d84b7d2c2bb69e83f91abef1ef112cb
sts-ubuntu <stsiwo@gmail.com>
2019-10-30 07:33:56 -0700

    added Tag common components

---
+++ Spa/src/UI/Base/Components/Tag/Tag.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './Tag.scss';
+import { TagPropType } from './types';
+
+const Tag: React.FunctionComponent<TagPropType> = (props: TagPropType) => {
+
+  return (
+    <div className="tags-item-wrapper" key={props.name}>
+      <i className="tags-item-name">{props.name}</i>
+      <button className="tags-item-cancel-btn">&#10006;</button>
+    </div>
+  );
+}
+
+export default Tag;
+
+
+
+
+
+
+
---
+++ Spa/src/UI/Base/Components/Tag/types.tsx
@@ -0,0 +1,4 @@
+export declare type TagPropType = {
+  name: string
+  withCancelBtn: boolean
+}
