1c1fa01d735097f50e375751ebbd800eaae5dc1c
sts-ubuntu <stsiwo@gmail.com>
2019-10-30 09:56:55 -0700

    update sass loader to accept css file

--- Spa/webpack.common.config.js
+++ Spa/webpack.common.config.js
@@ -44,7 +44,7 @@ module.exports = {
       },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
           {
-              test: /\.s[ac]ss$/i,
+              test: /\.(s[ac]ss|css)$/i,
               use: ['style-loader', 'css-loader', 'sass-loader']
           },
       {
