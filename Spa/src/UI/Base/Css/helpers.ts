d39ce6027b163a05bd0227f4a9904e2341c17f1f
sts-ubuntu <stsiwo@gmail.com>
2019-10-31 03:47:47 -0700

    added css helper function to identify overflown element from all dom element tree (haven't tested yet)

---
+++ Spa/src/UI/Base/Css/helpers.ts
@@ -0,0 +1,11 @@
+export const identifyElementOverflown = (element: HTMLElement): void => {
+  
+  if (element.clientHeight > window.innerHeight || element.clientWidth > window.innerWidth ) {
+    console.log(element)
+  }
+  if (element.hasChildNodes) {
+    for (var i = 0; i < element.childNodes.length; i++) {
+      identifyElementOverflown(element.childNodes[i] as HTMLElement)    
+    }
+  }
+}
