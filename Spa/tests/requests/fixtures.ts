0bffa046eb9b03eb7d94c0a758158be4e0dd6b39
sts-ubuntu <stsiwo@gmail.com>
2019-11-09 17:44:23 -0800

    added sample response fixtures

--- Spa/tests/requests/fixtures.ts
+++ Spa/tests/requests/fixtures.ts
@@ -58,6 +58,50 @@ export const blogGET500Response: AxiosResponse<ErrorResponseDataType> = {
   config: {},
 }
 
+export const internalServerError500Response: AxiosResponse<ErrorResponseDataType> = {
+  data: {
+    title: 'internal server error',
+    message: 'a filed must be xxxxxx',
+  } as ErrorResponseDataType,
+  status: 500,
+  statusText: 'Internal Server Error',
+  headers: {},
+  config: {},
+}
+
+export const badRequest400Response: AxiosResponse<ErrorResponseDataType> = {
+  data: {
+    title: 'bad request',
+    message: 'a filed must be xxxxxx',
+  } as ErrorResponseDataType,
+  status: 400,
+  statusText: 'Bad Request',
+  headers: {},
+  config: {},
+}
+
+export const unauthorized401Response: AxiosResponse<ErrorResponseDataType> = {
+  data: {
+    title: 'unauthorized 401 response title',
+    message: 'unauthorized 401 response message',
+  } as ErrorResponseDataType,
+  status: 401,
+  statusText: 'Unauthorized',
+  headers: {},
+  config: {},
+}
+
+export const notFound404Response: AxiosResponse<ErrorResponseDataType> = {
+  data: {
+    title: 'not found 404 response title',
+    message: 'not found 404 response message',
+  } as ErrorResponseDataType,
+  status: 404,
+  statusText: 'Not Found',
+  headers: {},
+  config: {},
+}
+
 declare global {
   interface Promise<T> {
     delay: (t: number) => Promise<any>
