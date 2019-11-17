09ab58ff2eb029150f2e2fe58c7af5b35a958768
sts-ubuntu <stsiwo@gmail.com>
2019-11-16 22:24:16 -0800

    impl and pass query string parse testing

--- Api1/Resources/parsers/QueryStringParser.py
+++ Api1/Resources/parsers/QueryStringParser.py
@@ -10,11 +10,11 @@ class QueryStringParser(object):
     def parse(self, args: MultiDict) -> Dict:
         argsDict: Dict = {}
         for key, value in args.lists():
-            # if array value (e.g., tag1,tag2,tag3), map as separate item in list
-            # might separate 'split' since other query string value might include ','
-            # for now, not separate but #DOUBT
-            argsDict[key] = {
-                    'value': value,
-                    'orOp': True
-                    }
+            if key == 'page' or key == 'limit':
+                argsDict[key] = value[0]
+            else:
+                argsDict[key] = {
+                        'value': value,
+                        'orOp': False
+                        }
         return argsDict
--- Api1/tests/resources/parsers/test_query_string_parser.py
+++ Api1/tests/resources/parsers/test_query_string_parser.py
@@ -1,23 +1,42 @@
 from Resources.parsers.QueryStringParser import QueryStringParser
 from werkzeug.datastructures import MultiDict
-from utils.util import printObject
+from utils.util import printObject, _printObject
 
 
 def test_query_string_parser():
 
     queryStringParser: QueryStringParser = QueryStringParser()
     dummyQueryStringMultiDict = MultiDict()
-    dummyQueryStringMultiDict['limit'] = '30'
-    dummyQueryStringMultiDict['offset'] = '0'
-    dummyQueryStringMultiDict['tags'] = 'tag1'
-    dummyQueryStringMultiDict['tags'] = 'tag2'
-    dummyQueryStringMultiDict['tags'] = 'tag3'
-    dummyQueryStringMultiDict['tags'] = 'tag4'
-    dummyQueryStringMultiDict['keyword'] = 'hey,hey'
-    dummyQueryStringMultiDict['startDate'] = '1996-10-15T00:05:32.000Z'
-    dummyQueryStringMultiDict['endDate'] = '1996-10-15T00:05:32.000Z'
+    dummyQueryStringMultiDict.add('limit', '30')
+    dummyQueryStringMultiDict.add('page', '1')
+    dummyQueryStringMultiDict.add('tags', 'tag1')
+    dummyQueryStringMultiDict.add('tags', 'tag2')
+    dummyQueryStringMultiDict.add('tags', 'tag3')
+    dummyQueryStringMultiDict.add('tags', 'tag4')
+    dummyQueryStringMultiDict.add('keyword', 'hey,hey')
+    dummyQueryStringMultiDict.add('startDate', '1996-10-15T00:05:32.000Z')
+    dummyQueryStringMultiDict.add('endDate', '1996-10-15T00:05:32.000Z')
 
     result = queryStringParser.parse(dummyQueryStringMultiDict)
 
-    printObject(result)
-    assert 0
+    expectedResult = {
+            'limit': '30',
+            'page': '1',
+            'tags': {
+                'value': ['tag1', 'tag2', 'tag3', 'tag4'],
+                'orOp': False
+                },
+            'keyword': {
+                'value': ['hey,hey'],
+                'orOp': False
+                },
+            'startDate': {
+                'value': ['1996-10-15T00:05:32.000Z'],
+                'orOp': False
+                },
+            'endDate': {
+                'value': ['1996-10-15T00:05:32.000Z'],
+                'orOp': False
+                },
+            }
+    assert result == expectedResult
