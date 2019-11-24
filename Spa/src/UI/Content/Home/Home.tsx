01faf25c808cacf971ee7eb3f7f4d04662e9c265
sts-ubuntu <stsiwo@gmail.com>
2019-11-23 23:09:53 -0800

    done Home component css

--- Spa/src/UI/Content/Home/Home.tsx
+++ Spa/src/UI/Content/Home/Home.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 import './Home.scss';
 import { GoSearch } from 'react-icons/go'
 import { CssPropertyAnimationType, searchInputAnimationStatus } from './types';
+import { BlogType } from 'domain/blog/BlogType';
+
+//test image
+const whiteAvatar = require('../../../../tests/data/images/white-1920x1280.jpg');
+const redImage = require('../../../../tests/data/images/red-girl-1920x1279.jpg');
+import range = require('lodash/range');
 
 
 const Home: React.FunctionComponent<{}> = (props: {}) => {
@@ -10,6 +16,9 @@ const Home: React.FunctionComponent<{}> = (props: {}) => {
   const searchInputRef = React.useRef<HTMLInputElement>()
   // search input display status
   const [currentSearchInputAnimationStatus, setSearchInputAnimationStatus] = React.useState<CssPropertyAnimationType>(searchInputAnimationStatus)
+  const [currentRecentBlogs, setRecentBlogs] = React.useState<BlogType>()
+  const [currentPopularBlogs, setPopularBlogs] = React.useState<BlogType>()
+  const [currentRecommendedBlogs, setRecommendedBlogs] = React.useState<BlogType>()
 
   const handleSearchIconClickEvent: React.EventHandler<React.MouseEvent<HTMLDivElement>> = (e) => {
     searchInputRef.current.style.width = currentSearchInputAnimationStatus.width.value[+currentSearchInputAnimationStatus.isNextDisplay] 
@@ -17,6 +26,22 @@ const Home: React.FunctionComponent<{}> = (props: {}) => {
     setSearchInputAnimationStatus({
       ...currentSearchInputAnimationStatus,
       isNextDisplay: !currentSearchInputAnimationStatus.isNextDisplay 
+    })
+  }
+
+  const renderBlogs: () => React.ReactNode = () => {
+    return range(5).map((num) => {
+      return (
+        <div className="blog-list-item-wrapper">
+          <img className="blog-list-item-img" src={redImage} alt="blog item" width="150px" height="100px"/>
+          <div className="blog-list-item-desc">
+            <h2 className="blog-list-item-desc-title">sample title might be a little bit longer</h2>
+              <p className="blog-list-item-desc-detail-main-tag">main tag</p>
+              <p className="blog-list-item-desc-detail-date">blog date</p>
+              <p className="blog-list-item-desc-author-name">author name</p>
+          </div>
+        </div>
+    );
     })
   }
 
@@ -32,18 +57,17 @@ const Home: React.FunctionComponent<{}> = (props: {}) => {
         <h1 className="home-slogan">Share Your Knowledge and Expand What You Can Do</h1>
       </div>
       <div className="home-blog-tabs-wrapper">
-        <button className="btn" >Recent</button>
-        <button className="btn" >Popular</button>
-        <button className="btn" >Recommended</button>
+        <button className="tab" >Recent</button>
+        <button className="tab" >Popular</button>
+        <button className="tab" >Recommended</button>
       </div>
-      <div className="home-featured-tag-blog-wrapper">
-        <h1 className="">featured tag blos list</h1>
+      <div className="home-blog-list-wrapper">
+        {renderBlogs()}
       </div>
-      <div className="home-recommendation-blog-wrapper">
-        <h1 className="">recommendation blog list if member</h1>
-      </div>
-      <div className="home-membership-promotion-wrapper">
-        <h1 className="">become member to post your blog</h1>
+      <div className="home-be-part-of-it-wrapper">
+        <h2 className="be-part-of-it-title">Be Part of It</h2>
+        <p className="be-part-of-it-desc">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <button className="btn">Join</button> 
       </div>
     </div>
   );
