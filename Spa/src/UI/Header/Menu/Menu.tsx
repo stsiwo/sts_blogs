0ca5554b8ea8458598d3bb9af52fab489dd6913a
sts-ubuntu <stsiwo@gmail.com>
2019-11-24 18:12:57 -0800

    update Header and Menu css & animation

--- Spa/src/UI/Header/Menu/Menu.tsx
+++ Spa/src/UI/Header/Menu/Menu.tsx
@@ -1,20 +1,45 @@
 import * as React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { StateType } from 'states/types';
 import './Menu.scss';
-import MenuCloseIcon from './MenuCloseIcon/MenuCloseIcon';
 import { useAuthContext } from 'Contexts/AuthContext/AuthContext';
+import { toggleNavBarActionCreator } from 'actions/creators';
+import { MdClose } from 'react-icons/md'
+import { useResponsiveComponent } from 'Hooks/ResponsiveComponentHook';
+import { useCssGlobalContext } from 'Contexts/CssGlobalContext/CssGlobalContext';
 
 
 const Menu: React.FunctionComponent<{}> = (props: {}) => {
 
   const isNavBarOpen = useSelector((state: StateType) => state.ui.isNavBarOpen)
-  const { auth } = useAuthContext()
+  const dispatch = useDispatch()
+  const currentScreenWidth = useResponsiveComponent()
+  const cssGlobal = useCssGlobalContext()
+  const ulRef = React.useRef<HTMLUListElement>()
 
-  return isNavBarOpen && (
-    <ul className="header-menu-ul" >
-      <MenuCloseIcon />
+  const { auth } = useAuthContext()
+  //const aniProps = useSpring({ width: isNavBarOpen ? currentScreenWidth + 'px' : '0px' })
+
+  const handleCloseNavBarClickEvent: React.EventHandler<React.MouseEvent<HTMLDivElement>> = (e) => {
+    dispatch(toggleNavBarActionCreator(false))
+  }
+
+  React.useEffect(() => {
+    if (currentScreenWidth < cssGlobal.laptopSize && ulRef.current.style) {
+      ulRef.current.style.width = isNavBarOpen ? '100%' : '0px'
+    }
+  })
+
+  return (
+    <ul className="header-menu-ul" ref={ulRef}>
+      {(currentScreenWidth < cssGlobal.laptopSize &&
+        <div className="header-menu-close-icon-row">
+          <div className="icon-wrapper" onClick={handleCloseNavBarClickEvent}>
+            <MdClose className="icon header-menu-close-icon" />
+          </div>
+        </div>
+      )}
       <li className="header-menu-li">
         <Link className="header-menu-li-link" to="./" >item1</Link>
       </li>
