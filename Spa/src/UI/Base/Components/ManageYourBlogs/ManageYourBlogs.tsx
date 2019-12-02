c2148afa843e37e4baa05c77eceb39fbc449eaca
sts-ubuntu <stsiwo@gmail.com>
2019-12-02 00:51:11 -0800

    fix MangeYurBlogs link

--- Spa/src/UI/Base/Components/ManageYourBlogs/ManageYourBlogs.tsx
+++ Spa/src/UI/Base/Components/ManageYourBlogs/ManageYourBlogs.tsx
@@ -9,7 +9,7 @@ const ManageYourBlogs: React.FunctionComponent<ManageYourBlogsPropType> = (props
     <div className="be-part-of-it-wrapper">
       <h2 className="be-part-of-it-title">Manage Your Blogs</h2>
       <p className="be-part-of-it-desc">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-      <Link to="/signup" className="link">
+      <Link to="/setting/blogs" className="link">
         <button className="btn">Explore</button>
       </Link>
     </div>
