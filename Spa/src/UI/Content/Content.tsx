4047735007dd8a8ba31e2093d657271b11bd64e4
sts-ubuntu <stsiwo@gmail.com>
2019-12-08 02:48:04 -0800

    add Switch route for fixing route error

--- Spa/src/UI/Content/Content.tsx
+++ Spa/src/UI/Content/Content.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Blog from './Blog/Blog';
 import BlogList from './BlogList/BlogList';
 import './Content.scss';
@@ -14,13 +14,15 @@ const Content: React.FunctionComponent<{}> = (props: {}) => {
 
   return (
     <section className="content-wrapper">
-      <Route path="/" exact component={Home} /> 
-      <Route path="/login" exact component={Login} /> 
-      <Route path="/signup" exact component={Signup} /> 
-      <Route path="/blogs" exact component={BlogList} /> 
-      <Route path="/blogs/:id" exact component={Blog} /> 
-      {/** siince setting has route, remove 'exact' **/}
-      <AuthRoute path="/setting" component={Setting} /> 
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/blogs" exact component={BlogList} />
+        <Route path="/blogs/:id" exact component={Blog} />
+        {/** siince setting has route, remove 'exact' **/}
+        <AuthRoute path="/setting" component={Setting} />
+      </Switch>
     </section>
   );
 }
