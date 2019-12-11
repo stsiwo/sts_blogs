e21bda3b6a912075c5995c8daf064d8a09873bd0
sts-ubuntu <stsiwo@gmail.com>
2019-12-11 10:41:12 -0800

    start implimenting selenium testing

--- IntegrationTesting/conftest.py
+++ IntegrationTesting/conftest.py
@@ -1,14 +1,31 @@
 import pytest
+from selenium import webdriver
 from typing import Dict
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+targetUrl = 'http://stsiwo.info'
+seleniumServerUrl = 'http://127.0.0.1:4444/wd/hub'
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     print('setup app fixture')
 
     app: Dict = {
-            "url": 'http://0.0.0.0:8080'
+            "url": targetUrl
             }
 
     yield app
     print('tear down app fixture')
+
+
+@pytest.fixture(scope="session")
+def chrome_driver(app):
+    print('setup chrome_driver fixture')
+    driver = webdriver.Remote(
+        command_executor=seleniumServerUrl,
+        desired_capabilities=DesiredCapabilities.CHROME
+        )
+    driver.get(app.get('url'))
+    yield driver
+    print('tear down chrome_driver fixture')
--- IntegrationTesting/test_sample.py
+++ IntegrationTesting/test_sample.py
@@ -1,22 +1,14 @@
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-def test_chrome(app):
+def test_chrome(chrome_driver):
     print('start test selenium')
-    driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
-        desired_capabilities=DesiredCapabilities.CHROME
-        )
+    assert 'STS' in chrome_driver.title
 
-    driver.get(app.get('url'))
-    assert 'STS' in driver.title
+    # elem = driver.find_element_by_name('p')  # Find the search box
+    # elem.send_keys('seleniumhq' + Keys.RETURN)
 
-    elem = driver.find_element_by_name('p')  # Find the search box
-    elem.send_keys('seleniumhq' + Keys.RETURN)
-
-    driver.quit()
+    chrome_driver.quit()
 
 
 # def test_firefox(sampleFixture):
