1fff5cc063e5237680a5e2c52f8f7a4656568be9
sts-ubuntu <stsiwo@gmail.com>
2019-12-11 08:18:08 -0800

    integrate spa with api1 (request)

--- Spa/src/configs/apiConfig.ts
+++ Spa/src/configs/apiConfig.ts
@@ -1,8 +1,7 @@
 import { AxiosRequestConfig } from "axios";
 
 export const apiConfig: AxiosRequestConfig = {
-  //baseURL: process.env.API1_URL,
-  baseURL: 'https://stsiwo.info', 
+  baseURL: process.env.API1_URL,
   timeout: 5000,
 
 }
