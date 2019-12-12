f67974f85e87b9068c69a1eae5a5509b65f835d6
sts-ubuntu <stsiwo@gmail.com>
2019-12-12 09:27:16 -0800

    fix testing container error: PIL 'pil' importerror error loading shared library libjpeg no such file or directory

--- Api1/entrypoint.testing.sh
+++ Api1/entrypoint.testing.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+export FLASK_ENV=testing
 export FLASK_APP=./run.py
+export PYTHONDONTWRITEBYTECODE=1
+rm -r ./migrations
 flask db init
 flask db migrate
 flask db upgrade
+flask seed roles
+flask seed tags
 python -m pytest 
