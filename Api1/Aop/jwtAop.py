629429465457a4d64f822107cbe778b93f281647
sts-ubuntu <stsiwo@gmail.com>
2019-12-17 20:11:15 -0800

    add expired jwt token handler (decorator)

---
+++ Api1/Aop/jwtAop.py
@@ -0,0 +1,12 @@
+from Configs.extensions import jwt
+from flask import jsonify
+
+
+@jwt.expired_token_loader
+def my_expired_token_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
